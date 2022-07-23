361c8e6add59f060085273e3f383aba70110d121
Moneshsoni <sonimonesh115@gmail.com>
2022-07-23 21:55:19 +0530

    add hardhat configuration

--- nft-marketpace/hardhat.config.js
+++ nft-marketpace/hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
+projectId = "a2b1ca3050dc4027a33822b65b1e71d3"
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks:{
+    hardhat:{
+      chainId: 1337
+    },
+    goerli:{
+      url:'https://goerli.infura.io/v3/${projectId}',
+      accounts: []
+    }
+  },
   solidity: "0.8.9",
 };
